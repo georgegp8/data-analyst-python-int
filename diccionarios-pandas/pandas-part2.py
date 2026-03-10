f0220bfaa16d667e35e3e78f880fc9cc522ba7e0
George Guerra <george.guerra@tecsup.edu.pe>
2026-03-10 15:39:12 -0500

    add third section to pandas part2

--- diccionarios-pandas/pandas-part2.py
+++ diccionarios-pandas/pandas-part2.py
@@ -55,3 +55,42 @@ print(cars[0:3])
 
 # Print out fourth, fifth and sixth observation
 print(cars[3:6])
+
+# ------------------------------------------------
+# Sección 3: loc e iloc (1)
+# ------------------------------------------------
+# Con loc y iloc puedes hacer prácticamente cualquier operación de selección de datos en
+# DataFrames que se te ocurra. loc se basa en etiquetas, lo que significa que tienes que
+# especificar filas y columnas en función de las etiquetas de fila y columna. iloc se basa en
+# índices enteros, por lo que tienes que especificar filas y columnas por su índice entero,
+# como hiciste en el ejercicio anterior.
+# Prueba los siguientes comandos para experimentar con loc y iloc para seleccionar
+# observaciones. Cada par de comandos da el mismo resultado.
+#
+# cars.loc['RU']
+# cars.iloc[4]
+#
+# cars.loc[['RU']]
+# cars.iloc[[4]]
+#
+# cars.loc[['RU', 'AUS']]
+# cars.iloc[[4, 1]]
+#
+# Como antes, se incluye código que importa los datos de los coches como un DataFrame de pandas.
+
+# Instrucciones:
+# - Utiliza loc o iloc para seleccionar la observación correspondiente a Japón como una serie.
+#   La etiqueta de esta fila es JPN, el índice es 2. Asegúrate de imprimir la serie resultante.
+# - Utiliza loc o iloc para seleccionar las observaciones de Australia y Egipto como un
+#   DataFrame. Puedes averiguar las etiquetas/índices de estas filas si inspeccionas cars.
+#   Asegúrate de imprimir el DataFrame resultante.
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col=0)
+
+# Print out observation for Japan
+print(cars.loc['JPN'])
+
+# Print out observations for Australia and Egypt
+print(cars.iloc[[1, 6]])
