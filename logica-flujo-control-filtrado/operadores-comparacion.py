9f2c870489af30b21ad67a3b30ad52b39fd83871
George Guerra <george.guerra@tecsup.edu.pe>
2026-03-14 13:00:35 -0500

    add new module flow control and filtering logic

---
+++ logica-flujo-control-filtrado/operadores-comparacion.py
@@ -0,0 +1,31 @@
+# ================================================
+# Lógica, Flujo de Control y Filtrado
+# Ejercicios: Operadores de Comparación
+# ================================================
+
+# ------------------------------------------------
+# Sección 1: Igualdad
+# ------------------------------------------------
+# Para comprobar si dos valores Python son iguales usas el operador de comparación ==.
+# Para comprobar la desigualdad, necesitas !=.
+# El operador < y > funcionan como de espera en Python: comprueban si el valor de la
+# izquierda es menor o mayor que el de la derecha respectivamente.
+# Por último, <= y >= comprueban si el miembro de la izquierda es menor o igual que
+# o mayor o igual que el miembro de la derecha.
+
+# Instrucciones:
+# - En el código de la muestra, expresiones como True == False ya están codificadas.
+#   Pruébalo y observa cuáles son los resultados.
+
+# Comparison of booleans
+print(True == False)
+
+# Comparison of integers
+print(-5 * 15 != 75)
+
+# Comparison of strings
+print("pyscript" == "pyscript")
+
+# Compare a boolean with an integer
+print(True == 1)
+
