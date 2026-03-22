4f2f62bd446be3f764625a0259d32f1fd3cce0b0
George Guerra <george.guerra@tecsup.edu.pe>
2026-03-21 21:50:42 -0500

    add first section to for file

---
+++ bucles/for.py
@@ -0,0 +1,30 @@
+# ================================================
+# Bucles
+# Ejercicios: for
+# ================================================
+
+# ------------------------------------------------
+# Sección 1: Bucle por una lista
+# ------------------------------------------------
+# Echa otro vistazo al bucle for que Hugo mostró en el vídeo:
+#
+#   fam = [1.73, 1.68, 1.71, 1.89]
+#   for height in fam :
+#       print(height)
+#
+# Como de costumbre, solo tienes que sangrar el código con 4 espacios para indicar a Python
+# qué código debe ejecutarse en el bucle for.
+#
+# La variable areas, que contiene la superficie de las distintas habitaciones de la casa,
+# ya está definida.
+
+# Instrucciones:
+# Escribe un bucle for que itere sobre todos los elementos de la lista areas e imprima
+# cada elemento por separado.
+
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Code the for loop
+for area in areas :
+    print(area)
