ec7f95bce165744af025d735cad5fc97f2820848
George Guerra <george.guerra@tecsup.edu.pe>
2026-03-21 21:51:40 -0500

    add second section to for file

--- bucles/for.py
+++ bucles/for.py
@@ -28,3 +28,30 @@ areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 # Code the for loop
 for area in areas :
     print(area)
+
+# ------------------------------------------------
+# Sección 2: Índices y valores (1)
+# ------------------------------------------------
+# Utilizar un bucle for para iterar por una lista solo te da acceso a cada elemento de la lista en cada
+# ejecución, uno tras otro. Si también quieres acceder a la información del índice, es decir, dónde se encuentra
+# el elemento de la lista sobre el que estás iterando, puedes utilizar enumerate().
+#
+# Como ejemplo, mira cómo se convirtió el bucle for del vídeo:
+#
+#   fam = [1.73, 1.68, 1.71, 1.89]
+#   for index, height in enumerate(fam) :
+#       print("person " + str(index) + ": " + str(height))
+
+# Instrucciones:
+# - Adapta el bucle for del código de ejemplo para utilizar enumerate() y utilizar dos variables de iteración.
+# - Actualiza la declaración print() para que en cada ejecución se imprima una línea de la forma
+#   "room x: y", donde X es el índice del elemento de la lista e Y es el elemento real de la lista, es decir, el
+#   área. Asegúrate de imprimir esta cadena exacta, con el espaciado correcto.
+
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Change for loop to use enumerate() and update print()
+for index, area in enumerate(areas) :
+    print("room " + str(index) + ": " + str(area))
+
