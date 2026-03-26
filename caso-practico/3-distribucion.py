322f88783edab99e1fda4d668c404c1d6c3a73fb
George Guerra <george.guerra@tecsup.edu.pe>
2026-03-25 23:33:43 -0500

    add fourth section to distribution file

--- caso-practico/3-distribucion.py
+++ caso-practico/3-distribucion.py
@@ -176,4 +176,65 @@ import matplotlib.pyplot as plt
 plt.plot(np_aw_t)
 plt.show()
 
+# ------------------------------------------------
+# Sección 4: Representa la distribución
+# ------------------------------------------------
+# Todas estas visualizaciones extravagantes nos han desviado. Aún tenemos que resolver el problema del
+# millón: ¿Cuáles son las probabilidades de que llegues a 60 escalones de altura en el Empire State Building?
+#
+# Básicamente, quieres conocer los puntos finales de todos los paseos aleatorios que has simulado. Estos
+# puntos finales tienen una determinado distribución que puedes visualizar con un histograma.
+#
+# Ten en cuenta que, si el código tarda demasiado en ejecutarse, puede que estés creando un histograma de
+# los datos equivocados.
+
+# Instrucciones:
+# - Para asegurarnos de que tenemos suficientes simulaciones, que se te vaya la cabeza. Simula el paseo
+#   aleatorio 500 veces.
+# - En np_aw_t, selecciona la última fila. Aquí está el punto final de los 500 paseos aleatorios que has
+#   simulado. Guarda esta matriz NumPy como ends.
+# - Utiliza plt.hist() para construir un histograma de ends. No olvides plt.show() para visualizar el
+#   gráfico.
+
+# Import numpy as np
+import numpy as np
+
+# Set the seed
+np.random.seed(123)
+
+# Initialize all_walks
+all_walks = []
+
+# Simulate random walk 500 times
+for i in range(500) :
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+
+        # Implement clumsiness
+        if np.random.rand() <= 0.001 :
+            step = 0
+
+        random_walk.append(step)
+    all_walks.append(random_walk)
+
+# Create and plot np_aw_t
+np_aw_t = np.transpose(np.array(all_walks))
+
+# Select last row from np_aw_t: ends
+ends = np_aw_t[-1,:]
+
+# Plot histogram of ends, display plot
+import matplotlib.pyplot as plt
+plt.hist(ends)
+plt.show()
+
 
