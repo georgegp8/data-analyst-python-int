7c8b2dac373837b9cf3834641ac61b7a7fd7e3a3
George Guerra <george.guerra@tecsup.edu.pe>
2026-03-25 23:31:20 -0500

    add second section to distribution file

--- caso-practico/3-distribucion.py
+++ caso-practico/3-distribucion.py
@@ -51,3 +51,67 @@ for i in range(5) :
 
 # Print all_walks
 print(all_walks)
+
+# ------------------------------------------------
+# Sección 2: Visualiza todos los paseos
+# ------------------------------------------------
+# all_walks es una lista de listas: cada sublista representa un único paseo aleatorio. Si conviertes esta lista de
+# listas en una matriz NumPy, podrás empezar a hacer gráficos interesantes. matplotlib.pyplot ya se ha
+# importado como plt.
+#
+# El bucle anidado for ya se ha programado; no te preocupes. De momento, céntrate en el código que viene
+# después de este bucle for.
+
+# Instrucciones:
+# - Utiliza np.array() para convertir all_walks en una matriz NumPy, np_aw.
+# - Intenta utilizar plt.plot() en np_aw. Incluye también plt.show() .¿Ya funciona?
+# - Transpón np_aw mediante la llamada a np.transpose() en np_aw. Llama al resultado np_aw_t. Llama a
+#   plt.plot() de nuevo con este np_aw_t y muéstralo. ¿Mejor?
+
+# Import numpy as np
+import numpy as np
+
+# Set the seed
+np.random.seed(123)
+
+# Initialize all_walks (don't change this line)
+all_walks = []
+
+# Simulate random walk 5 times
+for i in range(5) :
+
+    # Code from before
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+        random_walk.append(step)
+
+    # Append random_walk to all_walks
+    all_walks.append(random_walk)
+
+# Convert all_walks to NumPy array: np_aw
+np_aw = np.array(all_walks)
+
+# Plot np_aw and show
+import matplotlib.pyplot as plt
+plt.plot(np_aw)
+plt.show()
+
+# Clear the figure
+plt.clf()
+
+# Transpose np_aw: np_aw_t
+np_aw_t = np.transpose(np_aw)
+
+# Plot np_aw_t and show
+plt.plot(np_aw_t)
+plt.show()
+
